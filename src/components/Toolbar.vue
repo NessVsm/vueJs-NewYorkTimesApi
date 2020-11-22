ec08a29294c5c6650d663af351101b48aa88fd37
Vanessa Sales <vanessa.moreira@uniriotec.br>
2020-11-22 18:43:09 -0300

    (feat): creating toolbar component

---
+++ src/components/Toolbar.vue
@@ -0,0 +1,20 @@
+<template>
+    <md-toolbar md-elevation="0">
+        <md-field>
+            <label>Search</label>
+            <md-input v-model="initial"></md-input>
+            <md-icon>search</md-icon>
+
+        </md-field>
+    </md-toolbar>
+</template>
+
+<script>
+    export default {
+        name: "Toolbar"
+    }
+</script>
+
+<style scoped>
+
+</style>
