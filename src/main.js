2b9e96cacfe7feeb2c5904b896e00ee1a661309c
Vanessa Sales <vanessa.moreira@uniriotec.br>
2020-11-22 18:44:04 -0300

    (chore): importing material info

--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
+import 'vue-material/dist/vue-material.min.css'
+import 'vue-material/dist/theme/default.css'
+import VueMaterial from 'vue-material'
+Vue.use(VueMaterial)
 
 Vue.config.productionTip = false
-
 new Vue({
   render: h => h(App),
 }).$mount('#app')
