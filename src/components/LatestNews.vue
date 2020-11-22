a4d27bd687738d156f5476891509317f94fa497a
Vanessa Sales <vanessa.moreira@uniriotec.br>
2020-11-22 18:42:55 -0300

    (feat): creating latest news component

---
+++ src/components/LatestNews.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <md-card md-with-hover>
+            <md-ripple>
+                <md-card-header>
+                    <div class="md-title">Card with hover effect</div>
+                </md-card-header>
+
+                <md-card-content>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio itaque ea, nostrum odio. Dolores, sed accusantium quasi non.
+                </md-card-content>
+
+            </md-ripple>
+        </md-card>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "LatestNews"
+    }
+</script>
+
+<style scoped>
+
+</style>
