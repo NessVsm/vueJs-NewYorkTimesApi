3ac3ad0cff2194a568c5b0347a51dcbe795ebb93
Vanessa Sales <vanessa.moreira@uniriotec.br>
2020-11-25 18:59:10 -0300

    (feat): creating latestNews component

--- src/components/LatestNews.vue
+++ src/components/LatestNews.vue
@@ -1,26 +1,28 @@
 <template>
-    <div>
+    <div @click="onClick(newsUrl)">
         <md-card md-with-hover>
             <md-ripple>
                 <md-card-header>
-                    <div class="md-title">Card with hover effect</div>
+                    <div class="md-title">{{ headlineTitle }}</div>
                 </md-card-header>
-
-                <md-card-content>
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio itaque ea, nostrum odio. Dolores, sed accusantium quasi non.
-                </md-card-content>
-
+                <md-card-content> {{headlineDescription }}</md-card-content>
             </md-ripple>
         </md-card>
     </div>
 </template>
 
 <script>
-    export default {
-        name: "LatestNews"
+export default {
+    name: "LatestNews",
+    props: {
+        headlineTitle: String,
+        headlineDescription: String,
+        newsUrl: String
+    },
+    methods: {
+        onClick(url) {
+            window.location = url;
+        }
     }
+}
 </script>
-
-<style scoped>
-
-</style>
